d8c7d6b2cbce7a187cfbeeb2b04b00ebe614e096
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-10-08 00:17:41 -0400

    feat: Adding string interpolation learnings

--- main.dart
+++ main.dart
@@ -1,1 +1,22 @@
 /// THIS IS THE START
+
+// ====== Sección 1: Interpolación de Strings ======
+
+void main() {
+  Interpolacion();
+}
+
+void Interpolacion() {
+  String word = 'mira';
+  String upperWord = word.toUpperCase();
+  print(word + ' ' + upperWord);
+  print ('$word');
+}
+
+
+
+    // ====== Sección 1: Interpolación de Strings ======
+
+    // ====== Sección 1: Interpolación de Strings ======
+
+    // ====== Sección 1: Interpolación de Strings ======
